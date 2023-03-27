57adee9be9944e0428130bc8e57a8208ce560f3d
wangxinxing <393002848@qq.com>
2023-03-27 11:45:21 +0800

    updated:getFileUrl and getImageUrl

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,3 +1,4 @@
+import { ElevatorSharp } from "@mui/icons-material";
 import util from "digimaker-ui/util";
 export const getcustomPropetryConfig = (type:any)=>{
   let customPropeyConfig:any={
@@ -6,9 +7,26 @@ export const getcustomPropetryConfig = (type:any)=>{
   }
   return customPropeyConfig[type]
 }
-export const getFileUrl = (path:any)=>{
-  return util.washVariables(process.env.REACT_APP_ASSET_URL as string, {imagepath:path})
+export const getFileUrl = (path: any) => {
+  if (process.env.REACT_APP_ASSET_URL) {
+    return util.washVariables(process.env.REACT_APP_ASSET_URL as string, {imagepath:path})
+  } else {
+    return path
+  }
+  
 }
-export const getImageUrl = (path:any,isThumbnail?:boolean)=>{
-  return util.washVariables(isThumbnail?process.env.REACT_APP_THUMB_PATH as string:process.env.REACT_APP_ASSET_URL as string, {imagepath:path})
+export const getImageUrl = (path: any, isThumbnail?: boolean) => {
+  if (isThumbnail) {
+    if (process.env.REACT_APP_THUMB_PATH) {
+      return util.washVariables(process.env.REACT_APP_THUMB_PATH as string, {imagepath:path})
+    } else {
+      return path
+    }
+  } else {
+    if (process.env.REACT_APP_ASSET_URL) { 
+      return util.washVariables(process.env.REACT_APP_ASSET_URL as string, {imagepath:path})
+    } else {
+      return path
+    }
+  }
 }
